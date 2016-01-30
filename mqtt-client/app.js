8f42160f80e1cd1d3716746d9299a5d9e62a237e
Alex Pringle <alex@alexpringle.co.uk>
2016-01-30 16:22:39 +0000

    connect to mqtt-broker and sub to coffee/readings

--- mqtt-client/app.js
+++ mqtt-client/app.js
@@ -1,1 +1,19 @@
+READINGS_TOPIC = 'coffee/readings';
 
+var mqtt = require('mqtt');
+var client  = mqtt.connect('mqtt://mqtt-broker');
+ 
+client.on('error', function(error) {
+  console.log("MQTT error " + error);
+});
+
+client.on('connect', function () {
+  console.log('Connected to mqtt-broker');
+  client.subscribe(READINGS_TOPIC);
+  console.log('Subscribed to ' + READINGS_TOPIC);
+});
+
+ 
+client.on('message', function (topic, message) {
+  console.log(message.toString());
+});
