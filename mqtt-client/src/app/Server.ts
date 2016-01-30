997e32b559b88128a7633e4944dfa254e6225b4c
Lewis Taylor <lewis@fractalsoftware.co.uk>
2016-01-30 20:07:50 +0000

    mqtt-client inserting into postgresql database in container

--- mqtt-client/src/app/Server.ts
+++ mqtt-client/src/app/Server.ts
@@ -1,19 +1,36 @@
-var READINGS_TOPIC: string = 'coffee/readings';
-
 import mqtt = require("mqtt");
-var client  = mqtt.connect('mqtt://mqtt-broker');
+import pg = require("pg");
 
-client.on('error', function(error) {
+
+//TODO REMOVE PASSWORD 
+var dbUrl: string = "tcp://postgres:ilovecoffee@postgres:5432/c4di_coffee_web_dev";
+var READINGS_TOPIC: string = "coffee/readings";
+var mqttClient  = mqtt.connect('mqtt://mqtt-broker');
+
+mqttClient.on('error', function(error) {
     console.log("MQTT error " + error);
 });
-
-client.on('connect', function () {
+mqttClient.on('connect', function () {
     console.log('Connected to mqtt-broker');
-    client.subscribe(READINGS_TOPIC);
+    mqttClient.subscribe(READINGS_TOPIC);
     console.log('Subscribed to ' + READINGS_TOPIC);
 });
-
-
-client.on('message', function (topic, message) {
+mqttClient.on('message', function(topic, message) {
     console.log(message.toString());
+    var json = JSON.parse(message);
+    pg.connect(dbUrl, function(err, client, done) {
+		client.query(
+			'INSERT into readings (reading_kg, time, avg_since, pot_name, inserted_at, updated_at) VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+			[json['reading_kg'], json['time'], json['avg_since'], json['pot_name'], new Date(), new Date()],
+			function(err, result) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log('row inserted with id: ' + result.rows[0].id);
+				}
+				client.end();
+			});			
+		});
 });
+
+
