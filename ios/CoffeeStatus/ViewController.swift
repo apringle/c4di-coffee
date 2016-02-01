48ab6ad6ed3c6cf2ba1f7695b4fa35b6914caecd
Alex Pringle <alex@alexpringle.co.uk>
2016-02-01 13:30:20 +0000

    add base coffee status ios app

---
+++ ios/CoffeeStatus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoffeeStatus
+//
+//  Created by Alexander Pringle on 01/02/2016.
+//  Copyright © 2016 Alex Pringle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
